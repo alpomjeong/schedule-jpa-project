bd5ba4ab31bb7153473081df7ad743ca8dfa936e
alpomjeong <jsw00415@gmail.com>
2025-04-01 14:20:50 +0900

    init : git init
    깃저장소 지정

---
+++ src/test/java/com/example/schedulejpaproject/ScheduleJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulejpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
